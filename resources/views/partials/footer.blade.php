3b25fc538d4b8ec0787324f1e895733e926c1748
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-05 20:18:58 +0200

    FEAT create footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,14 @@
 <footer>
-
+    <div class="container">
+        <div class="row-mc">
+            <ul>
+                <li><a href="http:#"></a>Home</li>
+                <li><a href="http:#"></a>Shop</li>
+                <li><a href="http:#"></a>News</li>
+                <li><a href="http:#"></a>Contacts</li>
+                <li><a href="http:#"></a>About us</li>
+            </ul>
+            <img class="logo" src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
+        </div>
+    </div>
 </footer>
