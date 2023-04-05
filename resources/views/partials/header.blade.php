282b1eb2daa5248cab0f648401cc86eab5edb849
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-05 20:12:06 +0200

    FEAT header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,26 @@
 <header>
-    <h1>sono l'header</h1>
+    <div class="container">
+        <div class="row-mc">
+            <img class="logo" src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
+            <ul class="">
+                <li>
+                    <a href="#">
+                        home
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        shop
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        news
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </header>
